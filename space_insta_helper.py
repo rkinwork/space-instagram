763eb972bcfd9496eee72e02be63e374dd2cc19a
rkinwork <rkinwork@gmail.com>
2019-04-10 13:56:20 +0300

    Fix step 20/21. Replace print notifications with error raising functions

--- space_insta_helper.py
+++ space_insta_helper.py
@@ -1,5 +1,4 @@
 import os
-import pprint
 import time
 import glob
 import itertools
@@ -11,8 +10,6 @@ IMAGES_DIR = 'images'
 HUBBLE_HOST = 'http://hubblesite.org/api/v3/'
 ACCEPTABLE_IMAGE_FILES_EXTENSIONS = ['jpg', 'jpeg', 'pdf', 'png', 'bpm']
 
-pp = pprint.PrettyPrinter()
-
 
 def get_file_extension(filename):
     _, extension = filename.rsplit('.', 1)
@@ -23,8 +20,7 @@ def get_file_extension(filename):
 
 def download_picture(url, picture_name, image_store_path=IMAGES_DIR):
     response = requests.get(url)
-    if not response.ok:
-        return None
+    response.raise_for_status()
     os.makedirs(image_store_path, exist_ok=True)
     with open(os.path.join(image_store_path, picture_name), 'wb') as file:
         file.write(response.content)
@@ -32,27 +28,23 @@ def download_picture(url, picture_name, image_store_path=IMAGES_DIR):
 
 def fetch_spacex_last_launch():
     response = requests.get('https://api.spacexdata.com/v3/launches/latest')
-
+    response.raise_for_status()
     flight_number = response.json()['flight_number']
     for link_number, link in enumerate(sorted(response.json()['links']['flickr_images']), 1):
         download_picture(link, f"spacex_{flight_number}_{link_number}.jpg")
-    print('All images has been downloaded to images folder')
 
 
 def download_hubble_collection(collection_name):
     # http://hubblesite.org/api/v3/images/printshop
     response = requests.get(os.path.join(HUBBLE_HOST, f'images/{collection_name}'))
-    if not response.ok:
-        return None
-    print([picture_description['id'] for picture_description in response.json()])
+    response.raise_for_status()
     list(map(download_hubble_picture_by_image_id,
              [picture_description['id'] for picture_description in response.json()]))
 
 
 def hubble_image_urls_by_image_id(image_id):
     response = requests.get(os.path.join(HUBBLE_HOST, f'image/{image_id}'))
-    if not response.ok:
-        return None
+    response.raise_for_status()
     urls = [image_file_node['file_url'] for image_file_node in response.json()['image_files']]
     return sorted(urls)
 
@@ -60,15 +52,13 @@ def hubble_image_urls_by_image_id(image_id):
 def download_hubble_picture_by_image_id(image_id):
     high_res_picture_url = hubble_image_urls_by_image_id(image_id)[0]
     file_name = f"{image_id}.{get_file_extension(high_res_picture_url)}"
-    print(f"downloading {file_name}")
     download_picture(high_res_picture_url, file_name)
-    print("downloading finished")
 
 
 def get_instabot_instance(username=None, password=None):
     bot_username = username if username else os.getenv('SPACE_INSTAGRAM_LOGIN')
     bot_password = password if password else os.getenv('SPACE_INSTAGRAM_PASSWORD')
-    bot = Bot()
+    bot = Bot(verbosity=False)
     bot.login(username=bot_username, password=bot_password)
     return bot
 
@@ -78,19 +68,22 @@ def main():
     fetch_spacex_last_launch()
 
 
+def remove_temp_photos():
+    for file_path in glob.glob("images/*.REMOVE_ME"):
+        os.remove(file_path)
+
+
 def upload_photo_to_instagram(filename='', caption=''):
     pic = os.path.abspath(filename)
     bot = get_instabot_instance()
     bot.upload_photo(pic, caption=caption)
-    if bot.api.last_response.status_code != 200:
-        print(bot.api.last_response)
-    for file_path in glob.glob(f"{filename}*.REMOVE_ME"):
-        os.remove(file_path)
+    bot.api.last_response.raise_for_status()
 
 
 def upload_all_photos():
     images_to_publish = (glob.glob(f"images/*.{extension}") for extension in ACCEPTABLE_IMAGE_FILES_EXTENSIONS)
     for image_path in itertools.chain.from_iterable(images_to_publish):
-        print(image_path)
         upload_photo_to_instagram(image_path)
         time.sleep(60)
+
+    remove_temp_photos()
